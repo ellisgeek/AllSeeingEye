4c53dd2fa47a6b020d151e37d7d001b7f0d2b399
Elliott Saille <ellisgeek@live.com>
2013-12-13 11:17:36 +0000

    Began writing installation function (NOT TESTED)

--- allSeingEye.py
+++ allSeingEye.py
@@ -14,20 +14,53 @@ from os import W_OK
 from os import makedirs
 from os import path
 from os import environ
+from os import walk
 from shutil import rmtree
 from shutil import copy2
 from sys import exit
+from time import sleep
+from subprocess import call
 
 #Variables
-"""
-tempDir  = environ["TEMP"] + "\\allSeeingEye"
-tnsnames = "C:\\oracle\\product\\10.2.0\\client\\NETWORK\\ADMIN\\tnsnames.ora"
-tnsnamesTemp = tempDir + "\tnsnames.ora"
-"""
 tempDir  = environ["TEMP"] + "/allSeeingEye"
 tnsnamesTemp = tempDir + "/tnsnames.ora"
 tnsnames = "C:/oracle/product/10.2.0/client/NETWORK/ADMIN/tnsnames.ora"
+oraInstaller = "M:/INSTALL/Voyager 8/10203_client_vista-win7"
+installTemp = tempDir + "/oracle"
+setup = installTemp + "/setup.exe"
+compatMode = "VISTASP2"
 
+def compatabilityChange(path, mode, runasadmin, verbose):	
+	"""
+	Borrowed from http://techdiary-viki.blogspot.com/2011/03/script-to-set-compatibility-mode-of.html
+	Change the compatibility mode of a windows EXE
+	Valid Compatibility modes are:
+	WIN95:		Windows 95
+	WIN98:		Windows 98 / Windows ME
+	WINXPSP2:	Windows XP (Service Pack 2)
+	WINXPSP3:	Windows XP (Service Pack 3)
+	VISTARTM:	Windows Vista
+	VISTASP1:	Windows Vista (Service Pack 1)
+	VISTASP2:	Windows Vista (Service Pack 2)
+	WIN7RTM:	Windows 7
+	WIN8RTM:	Windows 8
+	"""
+
+	print("Processing path %s" % path)
+
+	files = []
+	for dirpath, dirnames, filenames in walk(path):
+		files.extend(filenames)
+		
+	exec_files = filter(lambda x: x.endswith('.exe'), files)
+	if verbose:
+		print("%d files to process" % len(exec_files))
+		print("Setting mode to %s" % mode)
+
+	for ef in exec_files:
+		if verbose:
+			print("Processing file %s" % path + '\\' + ef)
+		system('REG.EXE ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" /v "%s" /t REG_SZ /d "%s" /f' % (ef, mode))
 
 def confirm(prompt=None, resp=False):
     """prompts for yes or no response from the user. Returns True for yes and
@@ -68,13 +101,53 @@ def backup():
         try:
             copy2(tnsnames, tnsnamesTemp)
         except IOError as e:
-            print("({})".format(e))
+            print("\n")
+			print("({})".format(e))
+			print("\n")
+			confirm("Backup Failed!\nReturn to main menu?", True)
+			mainMenu()
         else:
          print("\nBackup Complete!\n")
     else:
         clear()
         print("Unable to access tnsnames.ora at:")
         print(tnsnames)
+    confirm("Return To main Menu?", True)
+    mainMenu()
+	
+def install():
+    clear()
+    print("Installing Oracle database client\n")
+	if confirm("Continue Installation?", True) == False:
+		clear()
+		print("Installation aborted")
+		sleep(2)
+		mainMenu()
+		
+	if path.exists(oraInstaller):
+		try:
+			copy2(oraInstaller, installTemp)
+		except IOError as e:
+			print("\n")
+			print("({})".format(e))
+			print("\n")
+			confirm("\nInstallation Failed!\nReturn to main menu?", True)
+			mainMenu()
+		else:
+			print("\nInstaller Copied Successfully!\n")
+	else:
+		confirm("\nFailed to Copy Installer!\nReturn to main menu?", True)
+	
+	if path.exists(setup):
+		compatabilityChange(setup, compatMode, True, False)
+	else:
+		clear()
+		print("Could not change compatability mode on:\n%s\n" % setup)
+		confirm("Return to main menu?", True)
+		mainMenu()
+	
+	call("%s" % setup, shell=True)
+	
     confirm("Return To main Menu?", True)
     mainMenu()
     
